60a4295392179d534c59928dc5cfce7d6abda0a6
Dinush Jayakody <dinushjay@gmail.com>
2023-12-04 16:45:30 +1100

    task manager include package laravel ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <title>Tasks Manager Application</title>
     <!-- Include Bootstrap CSS from CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="..." crossorigin="anonymous">
+    {{--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="..." crossorigin="anonymous">--}}
 </head>
 
 <body>
