654efb669740f5bf50cc69d0c4d1fa935fef0dbe
Dinush Jayakody <dinushjay@gmail.com>
2023-12-04 11:58:13 +1100

    task manager show task template styles

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
-@section('title', $task->title)
 
 @section('content')
-    <p>{{$task->title}}</p>
-    <p>{{ $task->description }}</p>
-    <p>{{ $task->completion_status }}</p>
-    <p>{{$task->created_at}}</p>
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title mb-4 text-center">Task Details</h5>
+                <p class="card-text">Task Title: {{$task->title}}</p>
+                <p class="card-text">Task Description: {{$task->description}}</p>
+                <p class="card-text">Task Status: {{$task->completion_status ? 'Completed' : 'Not Completed'}}</p>
+                <p class="card-text">Task Created At: {{$task->created_at}}</p>
+            </div>
+        </div>
+    </div>
 @endsection
